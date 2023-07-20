c694caffeb73ad025644ffe3ee3c3311ee929254
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-07-20 12:28:37 +0200

    added working list of task. NOT DONE YET

--- js/script.js
+++ js/script.js
@@ -11,11 +11,21 @@
     ];
 
     const render = () => {
-        
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
     const init = () => {
-        
+        render();
     };
 
     init()
