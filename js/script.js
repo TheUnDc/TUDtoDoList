461dfde4cfb156e5d38495d7f5fb899d922f7ea0
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-07-20 11:59:40 +0200

    added in js basic template of application

---
+++ js/script.js
@@ -0,0 +1,22 @@
+{
+    const tasks = [
+        {
+            content: "zadanie1",
+            done: false,
+        },
+        {
+            content: "zadanie2",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        
+    };
+
+    const init = () => {
+        
+    };
+
+    init()
+}
