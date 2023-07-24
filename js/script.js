ac9bc75bccf3621ee718f28dc3038c732cafc808
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-07-24 16:23:04 +0200

    formated code in html and JS

--- js/script.js
+++ js/script.js
@@ -6,35 +6,43 @@
     const addTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
+
         });
         render();
+
     };
 
     const removeTask = (index) => {
         tasks.splice(index, 1);
         render();
+
     };
 
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
+
     };
 
     const bindEvents = () => {
-        const removeButtons = document.querySelectorAll(".js-remove")
+        const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
-                removeTask(index)
+                removeTask(index);
+
             });
+
         })
 
-        const toggleDoneButtons = document.querySelectorAll(".js-done")
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
 
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
             toggleDoneButton.addEventListener("click", () => {
                 toggleTaskDone(index)
+
             });
+
         })
     };
 
@@ -46,11 +54,13 @@
 
         if (newTaskContent === "") {
             return;
+
         }
 
         addTask(newTaskContent);
         rawNewTaskContent.focus();
         rawNewTaskContent.value = "";
+
     };
 
     const render = () => {
@@ -63,12 +73,16 @@
                     <button class="js-done task__checkButton">${task.done ? "✔" : ""}</button>
                     <span ${task.done ? "class='task__done'" : ""}>${task.content}</span>
                     <button class="js-remove task__checkButton task__checkButton--deleteMark">🗑</button>
+
                 </li>
+
             `;
-        }
+
+        };
 
         task.innerHTML = htmlString; 
         bindEvents();
+        
     };
 
     const init = () => {
